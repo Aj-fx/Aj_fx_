670eeef6beab7ff2197dc6512d668edceb20f2b9
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2022-01-12 13:54:49 +0530

    Fix

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'private') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '🌼@' + uye.id.split('@')[0] + '\n';
+                mesaj += '🌟@' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
@@ -31,7 +31,7 @@ else if (Config.WORKTYPE == 'public') {
         mesaj = '';
         grup['participants'].map(async (uye) => {
             if (uye.isAdmin) {
-                mesaj += '🌼@' + uye.id.split('@')[0] + '\n';
+                mesaj += '🌟@' + uye.id.split('@')[0] + '\n';
                 jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
             }
         });
