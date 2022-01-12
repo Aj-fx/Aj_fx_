ee4f299bf6782ec5aabcfe3102ccb1dd70ed5ef0
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2022-01-12 13:45:52 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
-const fs = require("fs");
+consa fs = require("fs");
 const path = require("path");
 const events = require("./events");
 const raganork = require("./raganork");
