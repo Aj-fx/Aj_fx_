00bff07dc7a51478504b58f7834da70abe2824c9
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2022-01-13 12:56:22 +0530

    Fix

--- plugins/pmblock.js
+++ plugins/pmblock.js
@@ -7,7 +7,7 @@ const Config = require('../config')
 
 // Inbox Block System
 // This Plugins By Hisham Muhammed 
-const INBO = "INBO BLOCK NOT ALLOWED 🖕"
+const INBO = "INBO NOT ALLOWED 🦹"
  if (Config.INBO == 'true') {
 Hisham.addCommand({on: 'text', fromMe: false, delownsewcmd: false, onlyPm: true }, (async (message, match) => {
         let regexb1ichu = new RegExp('.')
