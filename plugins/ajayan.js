0ae8c7697363c6d2989a0a9fc75f2cdbdcd2b8b6
Aj-fx <97775454+Aj-fx1@users.noreply.github.com>
2022-01-15 12:24:52 +0530

    .

--- plugins/ajayan.js
+++ plugins/ajayan.js
@@ -20,7 +20,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
  *Bot zone: https://chat.whatsapp.com/L38gYwSPTBILW3K4kdvMY1*
 
- *github link : 👀*
+ *github link : ɢɪᴛʜᴜʙ ʟɪɴᴋ ᴜɴᴀᴠᴀɪʟᴀʙʟᴇ*
 
  *ᴀᴊ-ғxꫂ⁩..♡︎*
 `}) 
