388fdf62d118d25ef55aa45e0e13f593fa6b3bef
Aj-fx <97775454+Aj-fx1@users.noreply.github.com>
2022-01-15 12:27:09 +0530

    .

--- plugins/database.js
+++ plugins/database.js
@@ -26,7 +26,7 @@ var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '
       
       const buttonMessage = {
           contentText: ' ʜʏ ᴅᴜᴅᴇ....😘\n\n```BOT NAME:``` *'+Config.BOT+'*\n\n🃏 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🍒 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n🃏 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
-          footerText: '© Kaztroserv2',
+          footerText: '♡︎ Kaztroserv2',
           buttons: buttons,
           headerType: 1
       }
