ec4ffc1115036e9cdcc6144eccf2ea11b330a51b
Kaztro <97775454+Aj-fx1@users.noreply.github.com>
2022-01-17 14:40:50 +0530

    .

--- plugins/setvar.js
+++ plugins/setvar.js
@@ -1,4 +1,4 @@
-const Asena = require('../events');
+const Asena = require('../events'); 
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
