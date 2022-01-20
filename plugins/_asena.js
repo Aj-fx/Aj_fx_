2e94f3bc9456e859c8419e094c8d6f1e665d82be
Aj-fx <94107120+Aj-fx@users.noreply.github.com>
2022-01-21 00:50:31 +0530

    .

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,4 +1,4 @@
-/* Copyright (C) Yusuf Usta.
+/* Copyright (C) Yusuf Usta. 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Remoded by - Amal
