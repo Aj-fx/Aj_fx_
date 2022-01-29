b4f306c270f3e9487f61a75880afaaf9384f6bf9
Aj-fx7 <98457311+Aj-fx7@users.noreply.github.com>
2022-01-29 15:17:12 +0530

    .

--- plugins/ajayan.js
+++ plugins/ajayan.js
@@ -11,14 +11,11 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name Kaztroser*
 
-*Creator number : wa.me/918281440156?text=Hi%20Aj-fx%20bro.%20*
-
-
- *Efx audios : https://chat.whatsapp.com/FmZqZDpjgyv8f2jVzfcGwp*
+*Creator number : wa.me/918281440156?text=Hi%20Aj-fx%20bro%20*
      
  *Instagram id: https://instagram.com/_ajayan_007?utm_medium=copy_link*
 
- *Bot zone: https://chat.whatsapp.com/L38gYwSPTBILW3K4kdvMY1*
+ *Bot zone: https://chat.whatsapp.com/INAF26f54MMJtzhkmgchtV*
 
  *github link : ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ*
 
