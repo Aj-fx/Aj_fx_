b794b28146197c06cf029d20c99472792ab828ba
Aj-fx7 <98457311+Aj-fx7@users.noreply.github.com>
2022-01-29 15:40:22 +0530

    .

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,7 +1,7 @@
 /* Copyright (C) Yusuf Usta. 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Remoded by - Amal
+Remoded by - Ajfx
 */
 
 const Asena = require('../events');
