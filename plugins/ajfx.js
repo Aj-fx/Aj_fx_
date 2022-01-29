3ec9c8edcbfca3760710ac3d463f7e87a2c7ca09
Aj-fx7 <98457311+Aj-fx7@users.noreply.github.com>
2022-01-29 15:42:51 +0530

    Update ajfx.js

--- plugins/ajfx.js
+++ plugins/ajfx.js
@@ -12,7 +12,7 @@ var ddd = ggg.toString('utf-8')
 let whb = Config.WORKTYPE == 'public' ? false : true 
 
 WhatsAlexa.addCommand({pattern: 'list', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
-// send a buttons message! Amalser.
+// send a buttons message! Kaztroser.
     var plk_say = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 var plk_here = new Date().toLocaleDateString(get_localized_date)
