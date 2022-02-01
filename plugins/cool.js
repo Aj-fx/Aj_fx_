39749145d35f4c4284e3b1d45dd08043922b6d9e
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-01 13:04:40 +0530

    .

--- plugins/cool.js
+++ plugins/cool.js
@@ -7,17 +7,16 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'owner', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/G2m5gTt.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/zMa7aL8.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot owner Aj-fx💞*
 
-*Creator number : wa.me/918281440156?text=Hi%20Aj-fx%20bro%20*
+*Creator number : http://wa.me/918281440156*
 
  *Instagram id: https://instagram.com/_ajayan_007?utm_medium=copy_link*
 
- *Bot zone: https://chat.whatsapp.com/INAF26f54MMJtzhkmgchtV*
+ *Bot zone: https://chat.whatsapp.com/HfaRULUgc4SHpcWZjACAAU*
 
- *github link : ɢɪᴛʜᴜʙ ʟɪɴᴋ ᴜɴᴀᴠᴀɪʟᴀʙʟᴇ*
 
  *ᴀᴊ-ғxꫂ⁩..♡︎*
 `}) 
