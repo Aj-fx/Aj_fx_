49416a17946c7b5ad4beba2b57c853498bfa1baf
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-01 13:17:28 +0530

    Update ajayan.js

--- plugins/ajayan.js
+++ plugins/ajayan.js
@@ -7,17 +7,17 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/jDkKLYG.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/u76xdWZ.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name Kaztroser*
 
-*Creator number : wa.me/918281440156?text=Hi%20Aj-fx%20bro%20*
+*Creator number : http://wa.me/918281440156*
      
  *Instagram id: https://instagram.com/_ajayan_007?utm_medium=copy_link*
 
- *Bot zone: https://chat.whatsapp.com/INAF26f54MMJtzhkmgchtV*
+ *Bot zone: https://chat.whatsapp.com/HfaRULUgc4SHpcWZjACAAU*
 
- *github link : ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ*
+ *github link : https://github.com/Aj-fx/Kaztroser*
 
  *ᴀᴊ-ғxꫂ⁩..♡︎*
 `}) 
