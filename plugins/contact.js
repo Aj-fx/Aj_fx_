b7b26b6255b6443eed6db4e88e5d35ba041596cc
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-02 19:27:16 +0530

    Update contact.js

--- plugins/contact.js
+++ plugins/contact.js
@@ -9,9 +9,9 @@ Ktb.addCommand({ pattern: 'git ?(.*)', fromMe: false, desc: 'owner number' }, (a
 //coded by saidali
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Ajfx [OWNER]\n' // full name
-            + 'ORG:Ajfx;\n' // 
+            + 'FN:ᴀᴊғx [OWNER]\n' // full name
+            + 'ORG:ᴀᴊғx;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=918281440156:+91 8289968468\n' // WhatsApp ID + phone number
             + 'END:VCARD'
-await message.client.sendMessage(message.jid,{displayname: "Ajfx [OWNER]", vcard: vcard}, MessageType.contact)
+await message.client.sendMessage(message.jid,{displayname: "ᴀᴊғx [OWNER]", vcard: vcard}, MessageType.contact)
 }))
