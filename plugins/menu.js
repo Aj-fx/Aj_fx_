7ce0520c4f430fadab6405d17d6d5951a62eec8e
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-08 23:57:23 +0530

    Error fixed

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,63 +1,16 @@
-const asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const invite = "it sends details of invite"
-const group link = "it sends links"
-const Config = require('../config');
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
 
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
 
-if (Config.WORKTYPE == 'private') {
-        asena.addCommand({pattern: 'invite', fromMe: true, deleteCommand: true, desc: invite,}, (async (message, match) => {
+Asena.addCommand({pattern: 'group link', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var r_text = new Array ();
-    
-    r_text[1] = "*╔═════🎀ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ🎀═════╗*\n           \n*⚜═ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ═⚜*\n\n*ɢʀᴏᴜᴘ ʟɪɴɢ -  https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*\n*            *\n*╚══════🎀ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ🎀═════╝*\n\n*▷Creator: ᴀͥᴊͭᴀᷤʏᴀͫɴͤ*"
+    var skl = await axios.get("https://i.imgur.com/u76xdWZ.jpeg", { responseType: 'arraybuffer' })
 
-    
-    await message.client.sendMessage(
-        message.jid,(r_text[1]), MessageType.text);
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name Kaztroser*
+ *ᴋᴀᴢᴛʀᴏsᴇ ʙᴏᴛ: https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*
+`}) 
 
-    }));
-
-
-        asena.addCommand({pattern: 'group link', fromMe: true, deleteCommand: true, desc: group link,}, (async (message, match) => {
-
-        var r_text = new Array ();
-    
-        r_text[1] = "*Group link*\n           *\n💥═ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ ɢʀᴏᴜᴘ ʟɪɴɢ═💥*\n\n*💘 https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*\n*"
-
-    
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
-    
-        }));    
-
-    }
-    
-
-    if (Config.WORKTYPE == 'public') {
-        asena.addCommand({pattern: 'invite', fromMe: false, deleteCommand: true, desc: invite,}, (async (message, match) => {
-
-    var r_text = new Array ();
-    
-    r_text[1] = "*╔═════🎀ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ🎀═════╗*\n           \n*⚜═ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ═⚜*\n\n*ɢʀᴏᴜᴘ ʟɪɴɢ -  https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*\n*            *\n*╚══════🎀ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ🎀═════╝*\n\n*▷Creator: ᴀͥᴊͭᴀᷤʏᴀͫɴͤ*"
-
-    
-    await message.client.sendMessage(
-        message.jid,(r_text[1]), MessageType.text);
-
-    }));
-
-
-        asena.addCommand({pattern: 'group link', fromMe: false, deleteCommand: true, desc: group link,}, (async (message, match) => {
-
-        var r_text = new Array ();
-    
-        r_text[1] = "*Group link*\n           *\n💥═ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ ɢʀᴏᴜᴘ ʟɪɴɢ ═💥*\n\n*💘 https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*\n*"
-
-    
-        await message.client.sendMessage(
-            message.jid,(r_text[1]), MessageType.text);
-    
-        }));    
-
-    }
+}));
