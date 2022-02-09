83d458e82445bcb2ffae4f9d6b3fb3a3f594d883
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-09 16:55:03 +0530

    ᴀͥᴊͭᴀᷤʏᴀͫɴͤ

---
+++ plugins/mk.js
@@ -0,0 +1,16 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'group link', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
+
+    var skl = await axios.get("https://i.imgur.com/u76xdWZ.jpeg", { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*ᴋⷪᴀᷫᴢᷫᴛᷝʀⷭᴏᷝsⷶᴇᷞʀ*
+ *ᴋᴀᴢᴛʀᴏsᴇ ʙᴏᴛ ɢʀᴏᴜᴘ ʟɪɴᴋ: https://chat.whatsapp.com/EdukdzFc6suJNCs62aJB3f*
+`}) 
+
+}));
