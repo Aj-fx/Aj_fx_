5adb59cfb7f1dc1fbf4d63caa6d81253d1795f15
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-10 17:42:02 +0530

    wait

--- plugins/truecall.js
+++ plugins/truecall.js
@@ -1,66 +1,113 @@
-/* Credits: souravkl11, raganork-api
-(c) souravkl11 2022 All rights reserved
-*/
-const skl = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
-const fs = require('fs');
-const got = require("got");
-const axios = require('axios');
-const setting = require('../config');
+const New = require('../events');
 const Config = require('../config');
-const s = require('../config');
-var v = s.CHANNEL
-var need = "*_Need instagram link!_*";
-var downloading = "_Downloading_";
-var need_acc = "*_Need an instagram username!_*";
-var fail = "*_Download failed! Check your link and try again_*";
-var need_acc_s = "_Need an instagram username or link!_";
-let sourav = setting.WORKTYPE == 'public' ? false : true
-skl.addCommand({ pattern: 'insta ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
-var q = !msg.reply_message.message ? query[1] : msg.reply_message.message
-if (!q)  return await msg.client.sendMessage(msg.jid, '_Unable to read link from message!_', MessageType.text, {quoted: msg.data});
-if (q && !q.includes('instagram.com')) return await msg.client.sendMessage(msg.jid, need, MessageType.text, {quoted: msg.data});
-var getid = /(?:https?:\/\/)?(?:www\.)?(?:instagram\.com(?:\/.+?)?\/(p|reel|tv)\/)([\w-]+)(?:\/)?(\?.*)?$/
-var url = getid.exec(q)
-if (url != null) {
-var res = await raganork.query.getPost(url[0],v )
-if (res === "false") return await msg.client.sendMessage(msg.jid, fail, MessageType.text, {quoted: msg.data});
-else await msg.client.sendMessage(msg.jid, downloading, MessageType.text, {quoted: msg.data});
-var buffer = await raganork.query.skbuffer(res.links[0].url)
-if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, quoted: msg.data});
-if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, quoted: msg.data});
-}
-else if (url == null) {
-var linksplit = q.split('https://')[1]
-var res = await raganork.query.getPost('https://'+linksplit,v )
-if (res === "false") return await msg.client.sendMessage(msg.jid, fail, MessageType.text, {quoted: msg.data});
-else await msg.client.sendMessage(msg.jid, downloading, MessageType.text, {quoted: msg.data});
-var buffer = await raganork.query.skbuffer(res.links[0].url)
-if (res.links[0].url.includes('mp4')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.video, { mimetype: Mimetype.mp4, quoted: msg.data});
-if (res.links[0].url.includes('jpg')) return await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, quoted: msg.data});
+const image = require('../buffer');
+const {MessageType} = require('@adiwajshing/baileys');
+const Language = require('../language');
+const Lang = Language.getString('asena');
+let sourav = Config.WORKTYPE == 'public' ? false : true
+
+New.addCommand({pattern: 'hi ?(.*)', fromMe: sourav, dontAddCommandList: true}, (async (message, match) => {
+        var img = await image.skbuffer(Config.LOGOSK)
+        var CMD_HELP = '';
+        if (match[1] === '') {
+            New.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var match = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var mmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var match = [command.pattern];
+                    }
     
-}
-}));
-skl.addCommand({ pattern: 'ig ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
-    if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc, MessageType.text, {quoted: msg.data});
-    var res = await raganork.query.getStalk(query[1])
-    if (res === "false") return await msg.client.sendMessage(msg.jid, "_Username invalid!_", MessageType.text, {quoted: msg.data})
-    var buffer = await raganork.query.skbuffer(res.hd_profile_pic_url_info.url)
-    await msg.client.sendMessage(msg.jid, buffer, MessageType.image, { mimetype: Mimetype.jpg, caption: '_*Name:*_ ' + `${res.fullname}` + '\n _*Bio:*_ ' + `${res.biography}`+ '\n _*Private account:*_ ' + `${res.is_private} ` + '\n _*Followers:*_ ' + `${res.followers}` + '\n _*Following:*_ ' + `${res.following}` + '\n _*Posts:*_ ' + `${res.post_count}` + '\n _*Verified:*_ ' + `${res.is_verified} ` + '\n _*IGTV videos:*_ ' + `${res.total_igtv_videos}`, quoted: msg.data});
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + Lang.EXAMPLE + ': ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n\n';
+                    }
+                    if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.warn + '```\n\n'
+                    }
+                    if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                    }
+                    if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.usage + '```\n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                    if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n\n'
+                    }
+                    if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                        CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                    }
+                }
+            );
+            await message.client.sendMessage(
+                message.jid, Config.BOT + '\n\n  \n' + CMD_HELP, MessageType.text, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 777, status: 200, thumbnail: fs.readFileSync('./photo/Kaztroser.jpg'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/Kaztroser.jpg')}}}});
+        } 
+        else {
+            var CMD_HELP = '';
+            New.commands.map(
+                async (command) =>  {
+                    if (command.dontAddCommandList || command.pattern === undefined) return;
+                    try {
+                        var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşıiöç1234567890 ]*)/);
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2]
+                    } catch {
+                        var cmatch = [command.pattern];
+                    }
+                    if (cmmatch.endsWith(' ')) {
+                        var cmmatch = command.pattern.toString().match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2].replace(' ', '')
+                    }
+                    if (cmmatch == match[1]) {
+                        var HANDLER = '';
+    
+                        if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                            HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                        } else {
+                            HANDLER = '.';
+                        }
+                        if (command.desc == '' && !command.usage == '' && command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + Lang.EXAMPLE + ': ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n\n';
+                        }
+                        if (command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.warn + '```\n\n'
+                        }
+                        if (!command.desc == '' && !command.usage == '' && command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
+                        }
+                        if (!command.desc == '' && command.usage == '' && !command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if (command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.usage + '```\n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                        if  (command.desc == '' && command.usage == '' && command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n\n'
+                        }
+                        if  (!command.desc == '' && !command.usage == '' && !command.warn == '') {
+                            CMD_HELP += ' ' + (match.length >= 3 ? (HANDLER + mmatch) : command.pattern) + '\n' + ' ' + ' ```' + command.desc + '``` \n' + '* ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n' + '* ' + Lang.WARN + ':* ```' + command.warn + '```\n\n'
+                        }
+                    }
+                }
+            );
+            if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+            await message.client.sendMessage(
+                message.jid, Config.BOT + '\n\n  \n' + CMD_HELP, MessageType.text, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 777, status: 200, thumbnail: fs.readFileSync('./photo/Kaztroser.jpg'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/Kaztroser.jpg')}}}});
+        
+        }
     }));
-skl.addCommand({ pattern: 'story ?(.*)', fromMe: sourav,dontAddCommandList: true }, (async (msg, query) => {
-if (query[1] === '') return await msg.client.sendMessage(msg.jid, need_acc_s, MessageType.text, {quoted: msg.data});
-var user = query[1];
-var res = await raganork.query.getStory(user,v)
-if (res === "false") return await msg.client.sendMessage(msg.jid, "_Story not found!_", MessageType.text, {quoted: msg.data})
-var url = ''
-res.result.stories.map((result) => {
-url += result.url + ','});
-var que = url !== false ? url.split(',') : [];
-for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
-var get = got(que[i], {https: {rejectUnauthorized: false}});
-var stream = get.buffer();
-stream.then(async (video) => {
-await msg.client.sendMessage(msg.jid, video, MessageType.video, { mimetype: Mimetype.mp4, caption: '```Story of '+res.result.username + '```', quoted: msg.data});
-})};
-}));
