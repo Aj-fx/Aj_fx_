2244b2d62e2e57a9b8504499958ad6ef1e04c408
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-12 19:27:19 +0530

    Fix

--- plugins/random.js
+++ plugins/random.js
@@ -1,5 +1,5 @@
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'); 
 const fs = require('fs');
 const axios = require('axios');
 const NEED_WORD = "*Must Enter some Words*"
