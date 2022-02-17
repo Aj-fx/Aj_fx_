a32e99ffa83303a0d9744b12981f3b3045641d0b
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-17 20:39:38 +0530

    Fix

--- plugins/sql/greetings.js
+++ plugins/sql/greetings.js
@@ -6,12 +6,12 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
-const config = require('../../config');
+const config = require('../../config'); 
 const { DataTypes } = require('sequelize');
 
 const GreetingsDB = config.DATABASE.define('Greeting', {
     chat: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING, 
       allowNull: false
     },
     type: {
