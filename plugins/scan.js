6cb184700edd523d96ede3f35166f960c869cd3b
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-20 20:17:30 +0530

    Fix

--- plugins/scan.js
+++ plugins/scan.js
@@ -3,7 +3,7 @@ Telegram: t.me/phaticusthiccy
 Instagram: www.instagram.com/kyrie.baran
 */
 
-const Asena = require('../events');
+const Asena = require('../events'); 
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
