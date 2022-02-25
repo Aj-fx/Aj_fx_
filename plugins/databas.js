e18ac8001c385a04331bf966e6623f099ee56e03
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-25 09:43:26 +0530

    Fix

--- plugins/databas.js
+++ plugins/databas.js
@@ -26,7 +26,7 @@ var afnplk = '```⏱ Time :' + plk_say + '```\n\n ```📅 Date :' + plk_here + '
       ]
       
       const buttonMessage = {
-          contentText: ' ʜʏ ᴅᴜᴅᴇ....😈\n\n```BOT NAME:``` *'+Config.BOT+'*\n\n💌 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🧞 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n🌟 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
+          contentText: ' ʜʏ ᴅᴜᴅᴇ..✨\n\n```BOT NAME:``` *'+Config.BOT+'*\n\n💌 ᴛɪᴍᴇ   : ```' + plk_say + '```\n🧞 ᴅᴀᴛᴇ : ```' + plk_here + '```\n\n🌟 ᴄʟɪᴄᴋ ᴍᴇɴᴜ ᴀɴᴅ ᴇɴᴊᴏʏ ᴛʜᴇ ʙᴏᴛ\n',
           footerText: 'ᴀͥᴊͭᴀᷤʏᴀͫɴͤ',
           buttons: buttons,
           headerType: 1
