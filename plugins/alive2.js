2b2c107704066350eb7874b23fc5e2c67475c15d
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-25 13:04:46 +0530

    Update alive2.js

--- plugins/alive2.js
+++ plugins/alive2.js
@@ -21,6 +21,7 @@ var split = Config.KAZTRO_SER.split('/');
 let whb = Config.WORKTYPE == 'public' ? false : true
 
 Kaztroser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+var img = await image.query.skbuffer(Config.LOGOSK)
 // send a buttons message!credit Ajayan
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
