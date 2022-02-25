4d49e58685297e3cac3f2ff218e3b94a2e25b9e3
ᴀͥᴊͭᴀᷤʏᴀͫɴͤꫂ⁩ <94107120+Aj-fx@users.noreply.github.com>
2022-02-25 13:26:38 +0530

    Fix

--- plugins/alive2.js
+++ plugins/alive2.js
@@ -10,6 +10,7 @@ const config = require('../config')
 const axios = require('axios')
 const request = require('request');
 const os = require('os');
+const image = require('../buffer');
 var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
 var ggg = Buffer.from(clh.cd, 'base64')
 var ddd = ggg.toString('utf-8')
@@ -21,7 +22,7 @@ var split = Config.KAZTRO_SER.split('/');
 let whb = Config.WORKTYPE == 'public' ? false : true
 
 Kaztroser.addCommand({pattern: 'alive', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
-var img = await image.query.skbuffer(Config.LG_LOGO)
+var img = await image.query.skbuffer(Config.LOGOSK)
 // send a buttons message!credit Ajayan
     const buttons = [
         {buttonId: 'id1', buttonText: {displayText: sk1 }, type: 1},
