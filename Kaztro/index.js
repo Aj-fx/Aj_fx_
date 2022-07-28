156d5693f7082ad741cb7e534733670b5aa649ec
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-07-28 18:05:20 +0530

    Update index.js

--- Kaztro/index.js
+++ Kaztro/index.js
@@ -9,7 +9,6 @@ WhatsAsena - Yusuf Usta
 module.exports = {
     Base: require('./Base'),
     Message: require('./Message'),
-    StringSession: require('./StringSession'),
     ReplyMessage: require('./ReplyMessage'),
     Image: require('./Image'),
     Video: require('./Video')
