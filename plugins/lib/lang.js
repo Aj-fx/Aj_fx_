06d171dc597f40c793687c4d9af4eb96ffe0ff08
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-07-28 21:31:44 +0530

    Update lang.js

--- plugins/lib/lang.js
+++ plugins/lib/lang.js
@@ -1,6 +1,6 @@
 const {LANGUAGE,VERSION} = require('../../config');
 const {existsSync,readFileSync} = require('fs');
 var json = existsSync(__dirname+'/lang/' + LANGUAGE + '.json') ? JSON.parse(readFileSync(__dirname+'/lang/' + LANGUAGE + '.json')) : JSON.parse(readFileSync(__dirname+'/lang/en.json'));
-console.log("ABU-MD "+VERSION)
+console.log("KAZTRO "+VERSION)
 function getString(file) { return json['STRINGS'][file]; }
 module.exports = {language: json, getString: getString }
